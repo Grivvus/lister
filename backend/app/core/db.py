abcb112bda49cc80a52db83b45396cdc3520f7d4
Arseniy <kruchik.a04@gmail.com>
2024-05-30 16:55:41 +0400

    cinda fix imports

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,7 +1,8 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from settings import settings
+from app.models import Game
+from app.core.settings import settings
 
 
 async def init_db():
@@ -11,3 +12,13 @@ async def init_db():
     )
 
     await init_beanie(database=client.db_name)
+
+
+async def test_insert():
+    game1 = Game(
+        name="The Witcher 3",
+        descriptiot="the game about adventures Geralt of Rivia",
+        rate=10,
+        review="best action RPG ever"
+    )
+    await game1.insert()
--- backend/app/models.py
+++ backend/app/models.py
@@ -2,3 +2,9 @@ from beanie import Document, Indexed
 from pydantic import BaseModel
 import pymongo
 
+
+class Game(Document):
+    name: str
+    description: str
+    rate: int
+    review: str
