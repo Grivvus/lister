781f6f6630d09016e4cd986e9bc0861ec73b52bd
Arseniy <kruchik.a04@gmail.com>
2024-05-31 13:23:43 +0400

    start to developing api routes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 
+from app.api.routes import book_routes, game_routes, movie_routes
 
-def create_app():
-    return FastAPI(
-        title="lister api",
-        version="0.1.0",
-        debug=True,
-        docs_url="/api/docs"
-    )
+api_router = APIRouter()
+api_router.include_router(book_routes.router, prefix="/book", tags=["book"])
+api_router.include_router(game_routes.router, prefix="/game", tags=["game"])
+api_router.include_router(movie_routes.router, prefix="/movie", tags=["movie"])
