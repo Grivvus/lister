f7405faf00601e41ab9b3eb356ab833bf841fd9a
Arseniy <kruchik.a04@gmail.com>
2024-06-03 10:08:39 +0400

    made little changes to models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,29 +1,48 @@
+import datetime
+
 from beanie import Document, Indexed
 
 
 class GenericItem(Document):
+    """
+    model that represents generic item in  db
+    """
     name: Indexed(str)
+    user: Indexed(int)
     status: str  # надо бы сделать типо enum [not_sarted, in_progress, finished] smth like that
     rate: int | None
     review: str | None
-    genre: str | None
-
-
-class Game(GenericItem):
-
-    class Settings:
-        name = "games"
+    add_time: datetime.datetime
+    change_time: datetime.datetime
 
 
 class Book(GenericItem):
+    """
+    model that represents book entity
+    """
     author: str
+    book_genre: str | None
 
     class Settings:
         name = "books"
 
 
+class Game(GenericItem):
+    """
+    model that represents game entity
+    """
+    game_genre: str | None
+
+    class Settings:
+        name = "games"
+
+
 class Movie(GenericItem):
+    """
+    model that represents movie entity
+    """
     director: str | None
+    movie_genre: str | None
 
     class Settings:
         name = "movies"
