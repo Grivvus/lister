bb6461d417a8c93e4d1adb0b5472511d623d13aa
Arseniy <kruchik.a04@gmail.com>
2024-06-04 11:02:11 +0400

    remade python packages installation on poetry

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,2 +1,3 @@
 # starts backend service locally with debug info
+poetry shell
 uvicorn --factory app.main:fastapi_factory --reload --host 0.0.0.0 --port 8000
---
+++ tg_bot/scripts/start.sh
@@ -0,0 +1,1 @@
+poetry run src/main.py
