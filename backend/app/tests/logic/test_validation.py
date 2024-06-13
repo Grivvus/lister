59c7cd7fbe887f4976c5eba9885786bac6ad086d
Arseniy <kruchik.a04@gmail.com>
2024-06-13 14:11:34 +0400

    add tests to backend

---
+++ backend/app/tests/logic/test_validation.py
@@ -0,0 +1,35 @@
+import pytest
+
+from app.logic.validation import (
+    validate_status,
+    validate_rate,
+    validate_status_and_rate
+)
+
+
+def test_positive_validate_status():
+    assert validate_status("in progress") is True
+    assert validate_status("not started") is True
+    assert validate_status("finished") is True
+
+
+def test_negative_validate_status():
+    assert validate_status("alkdjfd") is False
+    assert validate_status("inprogress") is False
+    assert validate_status("IN PROGRESS") is False
+
+
+def test_validate_rate():
+    assert validate_rate(-1) is False
+    assert validate_rate(10) is True
+    assert validate_rate(0) is True
+    assert validate_rate(11) is False
+    assert validate_rate(3) is True
+
+
+def test_validate_status_and_rate():
+    assert validate_status_and_rate("in progress", 2) is False
+    assert validate_status_and_rate("not started", 2) is False
+    assert validate_status_and_rate("finished", None) is True
+    assert validate_status_and_rate("in progress", None) is True
+    assert validate_status_and_rate("finished", 5) is True
